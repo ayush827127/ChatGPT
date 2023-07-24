29329772b507b9c7520ad6b5e01dbf4077a86d28
Aayush Gupta <93078212+ayush827127@users.noreply.github.com>
2023-07-24 14:11:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const showTypingAnimation = () => {
     // Display the typing animation and call the getChatResponse function
     const html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img src="images/chatbot.jpg" alt="chatbot-img">
+                        <img src="chatbot.jpg" alt="chatbot-img">
                         <div class="typing-animation">
                             <div class="typing-dot" style="--delay: 0.2s"></div>
                             <div class="typing-dot" style="--delay: 0.3s"></div>
@@ -109,7 +109,7 @@ const handleOutgoingChat = () => {
 
     const html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img src="images/user.jpg" alt="user-img">
+                        <img src="user.jpg" alt="user-img">
                         <p>${userText}</p>
                     </div>
                 </div>`;
